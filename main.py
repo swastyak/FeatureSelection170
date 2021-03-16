90f2e0e26abbef069695838611373061c97a1a13
Swastyak Gaur <swastyak@gmail.com>
2021-03-16 12:48:33 -0700

    Working on backwards

--- main.py
+++ main.py
@@ -71,64 +71,33 @@ def feature_search_demo(data):
     return
 
 
+def feature_backwards_demo(data):
+    return
+
+
 def main():
     print("Welcome to Swastyak's Feature Search Algorithm.")
     fileName = input(
-        "Type in the name of file to test (1 for small, 2 for large): \n")
+        "Type in the name of file to test (1 for small, 2 for large, 3 for special small): \n")
     typeAlgorithm = input("Type the number of the algo you want to run: \n")
-    defaultSmall = "CS170_SMALLtestdata__13.txt"
-    defaultLarge = "CS170_largetestdata__66.txt"
     if fileName == "1":
-        fileName = defaultSmall
+        fileName = "CS170_SMALLtestdata__13.txt"
     if fileName == "2":
-        fileName = defaultLarge
+        fileName = "CS170_largetestdata__66.txt"
     if fileName == "3":
         fileName = "CS170_small_special_testdata__95.txt"
     # data = csv.reader(fileName, delimiter=' ', skipinitialspace=True)
-
+    print("Going to read " + fileName)
     data = pd.read_csv(fileName, delim_whitespace=True, header=None).values
     print("Timer will be turned on now.")
     start_time = time.time()
-    feature_search_demo(data)
+    if typeAlgorithm == "1":
+        feature_search_demo(data)
+    else:
+        feature_backwards_demo(data)
     print("Total runtime was " + str(time.time() - start_time) + " seconds.")
 
     # print(data)
 
 
 main()
-
-
-# def leave_one_out_cross_validation(data, current_set, feature_to_add):
-#     temp_data = copy.deepcopy(data)
-#     row, col = data.shape
-#     number_correctly_classified = 0
-#     for iter in range(1, col):
-#         if iter not in current_set and iter != feature_to_add:
-#             temp_data[:, iter] = 0
-#
-#     for i in range(0, row):
-#         object_to_classify = temp_data[i, 1:]
-#         label_object_to_classify = temp_data[i, 0]
-#         nearest_neighbor_distance = sys.maxsize
-#         nearest_neighbor_location = sys.maxsize
-#
-#         for k in range(0, row):
-#             if i != k:
-#                 # print("Ask if " + str(i) + " is nearest neighbor with " + str(k))
-#                 distance = math.sqrt(sum(pow(object_to_classify - temp_data[k, 1:], 2)))
-#                 if distance < nearest_neighbor_distance:
-#                     nearest_neighbor_distance = distance
-#                     nearest_neighbor_location = k
-#                     nearst_neighbor_label = temp_data[nearest_neighbor_location, 0]
-#                 # print(distance)
-#         if label_object_to_classify == temp_data[nearest_neighbor_location, 0]:
-#             number_correctly_classified += 1
-#         # print("Object " + str(i) + " is class" + str(label_object_to_classify))
-#         # print("It's nearest neighbor is " + str(nearest_neighbor_location) +
-#         #      " which is in class " + str(temp_data[nearest_neighbor_location, 0]))
-#
-#         # print(object_to_classify)
-#         # print("Looping over i, at the " + str(i) + " location.")
-#         # print("The " + str(i) + "th object is in class " + str(label_object_to_classify))
-#     accuracy = number_correctly_classified / row
-#     return accuracy
