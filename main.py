e9bd7d8a2f80b9188f9f107d4d5caf3c9d09b731
Swastyak Gaur <swastyak@gmail.com>
2021-03-15 22:12:15 -0700

    forward search working

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
     temp_data = copy.deepcopy(data)
     row, col = data.shape
     number_correctly_classified = 0
+    print("Printing current set." + str(current_set))
+    # print("Printing current feature to add." + str(feature_to_add))
+
     for iter in range(1, col):
         if iter not in current_set and iter != feature_to_add:
             temp_data[:, iter] = 0
@@ -22,25 +25,16 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
         label_object_to_classify = temp_data[i, 0]
         nearest_neighbor_distance = sys.maxsize
         nearest_neighbor_location = sys.maxsize
-
+        nearest_neighbor_label = 0
         for k in range(0, row):
             if i != k:
-                # print("Ask if " + str(i) + " is nearest neighbor with " + str(k))
                 distance = math.sqrt(sum(pow(object_to_classify - temp_data[k, 1:], 2)))
                 if distance < nearest_neighbor_distance:
                     nearest_neighbor_distance = distance
                     nearest_neighbor_location = k
-                    nearst_neighbor_label = temp_data[nearest_neighbor_location, 0]
-                # print(distance)
-        if label_object_to_classify == temp_data[nearest_neighbor_location, 0]:
+                    nearest_neighbor_label = temp_data[nearest_neighbor_location, 0]
+        if label_object_to_classify == nearest_neighbor_label:
             number_correctly_classified += 1
-        # print("Object " + str(i) + " is class" + str(label_object_to_classify))
-        # print("It's nearest neighbor is " + str(nearest_neighbor_location) +
-        #      " which is in class " + str(temp_data[nearest_neighbor_location, 0]))
-
-        # print(object_to_classify)
-        # print("Looping over i, at the " + str(i) + " location.")
-        # print("The " + str(i) + "th object is in class " + str(label_object_to_classify))
     accuracy = number_correctly_classified / row
     return accuracy
 
@@ -48,24 +42,21 @@ def leave_one_out_cross_validation(data, current_set, feature_to_add):
 def feature_search_demo(data):
     row, col = data.shape
     currSetFeatures = set()
-    # print(type(currSetFeatures))
-    # for i in range(1, row):
-    for i in range(1, col):
+    for i in range(0, col-1):
         print("On the " + str(i) + " th level of the search tree")
         global featureToAdd
         featureToAdd = set()
         bestAccuracySoFar = 0
-        for k in range(1, col):
-            if k not in currSetFeatures:
-                print("Consider expanding the " + str(k) + " feature.")
+        for k in range(0, col-1):
+            if k + 1 not in currSetFeatures:
+                print("Consider expanding the " + str(k + 1) + " feature.")
                 accuracy = leave_one_out_cross_validation(data, currSetFeatures, k+1)
                 if accuracy > bestAccuracySoFar:
                     bestAccuracySoFar = accuracy
-                    featureToAdd = k
-        # currSetFeatures[i] = featureToAdd
+                    featureToAdd = k + 1
         currSetFeatures.add(featureToAdd)
-        print("On level " + str(i) + " i added feature " + str(featureToAdd) + " to current set")
-        print(bestAccuracySoFar)
+        print("On level " + str(i + 1) + " i added feature " + str(featureToAdd) + " to current set")
+        print("This is best accuracy so far:" + str(bestAccuracySoFar))
     return
 
 
@@ -87,8 +78,43 @@ def main():
     data = pd.read_csv(fileName, delim_whitespace=True, header=None).values
 
     feature_search_demo(data)
-    print(type(data))
     # print(data)
 
 
 main()
+
+
+# def leave_one_out_cross_validation(data, current_set, feature_to_add):
+#     temp_data = copy.deepcopy(data)
+#     row, col = data.shape
+#     number_correctly_classified = 0
+#     for iter in range(1, col):
+#         if iter not in current_set and iter != feature_to_add:
+#             temp_data[:, iter] = 0
+#
+#     for i in range(0, row):
+#         object_to_classify = temp_data[i, 1:]
+#         label_object_to_classify = temp_data[i, 0]
+#         nearest_neighbor_distance = sys.maxsize
+#         nearest_neighbor_location = sys.maxsize
+#
+#         for k in range(0, row):
+#             if i != k:
+#                 # print("Ask if " + str(i) + " is nearest neighbor with " + str(k))
+#                 distance = math.sqrt(sum(pow(object_to_classify - temp_data[k, 1:], 2)))
+#                 if distance < nearest_neighbor_distance:
+#                     nearest_neighbor_distance = distance
+#                     nearest_neighbor_location = k
+#                     nearst_neighbor_label = temp_data[nearest_neighbor_location, 0]
+#                 # print(distance)
+#         if label_object_to_classify == temp_data[nearest_neighbor_location, 0]:
+#             number_correctly_classified += 1
+#         # print("Object " + str(i) + " is class" + str(label_object_to_classify))
+#         # print("It's nearest neighbor is " + str(nearest_neighbor_location) +
+#         #      " which is in class " + str(temp_data[nearest_neighbor_location, 0]))
+#
+#         # print(object_to_classify)
+#         # print("Looping over i, at the " + str(i) + " location.")
+#         # print("The " + str(i) + "th object is in class " + str(label_object_to_classify))
+#     accuracy = number_correctly_classified / row
+#     return accuracy
