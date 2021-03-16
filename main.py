a150287b2c19984c6f6839fa1aec7dc09b7ca7ea
Swastyak Gaur <swastyak@gmail.com>
2021-03-15 21:14:26 -0700

    Assign cols to 0 if not use

--- main.py
+++ main.py
@@ -4,25 +4,45 @@ import numpy as np
 import csv
 import copy
 import sys
+import math
 
 # data: first val row index, 2nd value col index
 
 
 def leave_one_out_cross_validation(data, current_set, feature_to_add):
-    train_data = copy.deepcopy(data)
+    temp_data = copy.deepcopy(data)
     row, col = data.shape
+    number_correctly_classified = 0
+    for iter in range(1, col):
+        if iter not in current_set and iter != feature_to_add:
+            temp_data[:, iter] = 0
+
     for i in range(0, row):
-        object_to_classify = train_data[i, 1:]
-        label_object_to_classify = train_data[i, 0]
+        object_to_classify = temp_data[i, 1:]
+        label_object_to_classify = temp_data[i, 0]
         nearest_neighbor_distance = sys.maxsize
         nearest_neighbor_location = sys.maxsize
+
         for k in range(0, row):
             if i != k:
-                print("Ask if " + str(i) + " is nearest neighbor with " + str(k))
+                # print("Ask if " + str(i) + " is nearest neighbor with " + str(k))
+                distance = math.sqrt(sum(pow(object_to_classify - temp_data[k, 1:], 2)))
+                if distance < nearest_neighbor_distance:
+                    nearest_neighbor_distance = distance
+                    nearest_neighbor_location = k
+                    nearst_neighbor_label = temp_data[nearest_neighbor_location, 0]
+                # print(distance)
+        if label_object_to_classify == temp_data[nearest_neighbor_location, 0]:
+            number_correctly_classified += 1
+        # print("Object " + str(i) + " is class" + str(label_object_to_classify))
+        # print("It's nearest neighbor is " + str(nearest_neighbor_location) +
+        #      " which is in class " + str(temp_data[nearest_neighbor_location, 0]))
+
         # print(object_to_classify)
         # print("Looping over i, at the " + str(i) + " location.")
         # print("The " + str(i) + "th object is in class " + str(label_object_to_classify))
-    return random.random()
+    accuracy = number_correctly_classified / row
+    return accuracy
 
 
 def feature_search_demo(data):
